a61d5c51d7556e655f07e4e2bfbd0c1eb17f8bf3
Miroshnichenko Sergii <miroshnichenkos89@gmail.com>
2022-09-19 15:24:45 +0300

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Sergii Miroshnichenko on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
